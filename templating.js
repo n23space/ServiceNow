b7542428b887fcd8cc070f8a01fcf445a52a04ed
n23space <n23space@gmail.com>
2019-06-18 12:09:07 +0200

    Update templating.js

--- templating.js
+++ templating.js
@@ -1,15 +1,17 @@
 function templating(str, obj){
-		if (!str) return;
-		if (!obj) return str;
-		if (typeof str != 'string' || typeof obj != 'object') return str;
+	if (!str) return;
+	if (!obj) return str;
+	if (typeof str != 'string' || typeof obj != 'object') return str;
 		
-		var regex = /(?:\$\{)(.+?)(?:\})/g;
-		return str.replace(regex, replceTemp);
-		function replceTemp(rap, key, pos, fulstr){
-			if (!key) return rap;
-			if (obj && obj.hasOwnProperty(key)){
-				return obj[key];
-			}
-			return rap;
+	var regex = /(?:\$\{)(.+?)(?:\})/g;
+	
+	return str.replace(regex, replceTemp);
+
+	function replceTemp(rap, key, pos, fulstr){
+		if (!key) return rap;
+		if (obj && obj.hasOwnProperty(key)){
+			return obj[key];
 		}
+		return rap;
 	}
+}
