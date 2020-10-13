a30146c772fb8ab69ec1e7d78a24e765e3dc76fb
n23space <n23space@gmail.com>
2020-10-13 18:11:21 +0200

    Update runScheduledJob.js

--- runScheduledJob.js
+++ runScheduledJob.js
@@ -1,9 +1,8 @@
-function scheduleImportJob() {
-  var jobId = '4f54eb681bac9c10780843f28d4bcb38';
-  var gr = new GlideRecord('sysauto_script');
-  if (!gr.get(jobId)) {
-    throw new Error('Unable to find import job');
-  }
-  gs.executeNow(gr);
+function runScheduleJob(jobId) {
+	var autoScriptGr = new GlideRecord('sysauto_script');
+	if (!autoScriptGr.get(jobId)) {
+		throw new Error('Unable to find scheduled job');
+	}
+	gs.executeNow(autoScriptGr);
 }
-scheduleImportJob();
+runScheduleJob('4f54eb681bac9c10780843f28d4bcb38');
